fe375d32cc7ac24812f621cd9354650c28c3b04c
Carolin Suresh <carolinsuresh04@gmail.com>
2026-03-13 19:53:57 +0530

    Updated serial.reader file

--- serial_reader.py
+++ serial_reader.py
@@ -1,39 +1,49 @@
+
+
 import serial
 import requests
 
 PORT = "COM5"
 BAUD = 115200
 
-ser = serial.Serial(PORT, BAUD)
+ser = serial.Serial(PORT, BAUD, timeout=1)
 
-print("Reading LoRa data...")
+print("Reading LoRa data...\n")
 
 while True:
+    try:
+        line = ser.readline().decode(errors='ignore').strip()
 
-    line = ser.readline().decode().strip()
+        if line:
+            print("SERIAL:", line)
 
-    if "MLDATA:" in line:
+        # Look for machine-readable line
+        if "MLDATA:" in line:
 
-        data = line.split("MLDATA:")[1]
-        values = data.split(",")
+            data = line.split("MLDATA:")[1]
+            values = data.split(",")
 
-        ph = float(values[0])
-        turb = float(values[1])
-        temp = float(values[2])
-        do = float(values[3])
-        bod = float(values[4])
+            ph = float(values[0])
+            turb = float(values[1])
+            temp = float(values[2])
+            do = float(values[3])
+            bod = float(values[4])
 
-        payload = {
-            "pH": ph,
-            "Turbidity (NTU)": turb,
-            "Temperature (°C)": temp,
-            "DO (mg/L)": do,
-            "BOD (mg/L)": bod
-        }
+            payload = {
+                "pH": ph,
+                "Turbidity (NTU)": turb,
+                "Temperature (°C)": temp,
+                "DO (mg/L)": do,
+                "BOD (mg/L)": bod
+            }
 
-        res = requests.post(
-            "http://127.0.0.1:5000/predict",
-            json=payload
-        )
+            res = requests.post(
+                "http://127.0.0.1:5000/predict",
+                json=payload
+            )
 
-        print(res.json())
+            print("ML RESULT:", res.json())
+            print("-------------------------")
+
+    except Exception as e:
+        print("Error:", e)
