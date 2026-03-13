603474b3374da3001328e9ed403a4b247779b419
Carolin Suresh <carolinsuresh04@gmail.com>
2026-03-13 20:04:35 +0530

    updated files

--- serial_reader.py
+++ serial_reader.py
@@ -1,5 +1,3 @@
-
-
 import serial
 import requests
 
@@ -11,39 +9,42 @@ ser = serial.Serial(PORT, BAUD, timeout=1)
 print("Reading LoRa data...\n")
 
 while True:
+
     try:
         line = ser.readline().decode(errors='ignore').strip()
 
         if line:
             print("SERIAL:", line)
 
-        # Look for machine-readable line
-        if "MLDATA:" in line:
+        # Parse TX format: Sent: x,x,x,x,x,x
+        if "Sent:" in line:
 
-            data = line.split("MLDATA:")[1]
+            data = line.split("Sent:")[1].strip()
             values = data.split(",")
 
-            ph = float(values[0])
-            turb = float(values[1])
-            temp = float(values[2])
-            do = float(values[3])
-            bod = float(values[4])
+            if len(values) >= 5:
 
-            payload = {
-                "pH": ph,
-                "Turbidity (NTU)": turb,
-                "Temperature (°C)": temp,
-                "DO (mg/L)": do,
-                "BOD (mg/L)": bod
-            }
+                ph = float(values[0])
+                turb = float(values[2])
+                temp = float(values[4])
+                do = float(values[3])
+                bod = float(values[1])
 
-            res = requests.post(
-                "http://127.0.0.1:5000/predict",
-                json=payload
-            )
+                payload = {
+                    "pH": ph,
+                    "Turbidity (NTU)": turb,
+                    "Temperature (°C)": temp,
+                    "DO (mg/L)": do,
+                    "BOD (mg/L)": bod
+                }
 
-            print("ML RESULT:", res.json())
-            print("-------------------------")
+                res = requests.post(
+                    "http://127.0.0.1:5000/predict",
+                    json=payload
+                )
+
+                print("ML RESULT:", res.json())
+                print("---------------------------")
 
     except Exception as e:
         print("Error:", e)
